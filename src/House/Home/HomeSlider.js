931bb8587f60a4cebc1b2606b76b76c15d3e452c
Ropio-Derrick <rodetuk@gmail.com>
2023-01-22 18:36:56 +0300

    navfoot

--- src/House/Home/HomeSlider.js
+++ src/House/Home/HomeSlider.js
@@ -11,20 +11,20 @@ function HomeSlider() {
   const AutoplaySlider = withAutoplay(AwesomeSlider);
 
   return (
-    <AutoplaySlider className='h-[540px] xs-full sm:h-full top-0 '
+    <AutoplaySlider className='h-[540px]  xs:h-[400px] sm:h-[400px]  top-0 '
     play={true}
     cancelOnInteraction={false} // should stop playing on user interaction
     interval={6000}
   >
-    <div className='background top-0 '>
-      <img src={Imga} className='  background bg-left-bottom' />
+    <div className='background top-0 h-full'>
+      <img src={Imga} className='  background bg-left-bottom h-full ' />
     </div>
-    <div className='background  top-0 ' >
-      <img src={Imgb} className='  background bg-right-bottom' />
+    <div className='background  top-0   h-full' >
+      <img src={Imgb} className='  background bg-right-bottom h-full ' />
 
     </div>
-    <div className=' background  top-0 ' >
-      <img src={Imgc} className='background   bg-center-bottom' />
+    <div className=' background  top-0  h-full' >
+      <img src={Imgc} className='background h-full  bg-center-bottom' />
 
     </div>
 
--- src/House/Navbar/Navbar.js
+++ src/House/Navbar/Navbar.js
@@ -62,7 +62,7 @@ const Navbar =()=>{
     const [toggleMenu,setToggleMenu] = useState(false)
 
     return(
-        <nav className="w-full flex md:justify-center justify-between items-center p-4 ">
+        <nav className="w-full flex md:justify-center justify-between items-center p-4  sm:p-[7px] xs:p-[7px] ">
             <div className="md:flex-[0.5] flex-initial justify-center items-center ">
                 <img src={Logom}  alt="Logo" className="w-32 cursor-pointer" />
             </div>
